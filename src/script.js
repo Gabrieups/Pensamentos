49e4bd50a30a2571b4eb05ac3e691cc1323f971d
Gabrieups <paixaogabriel498@gmail.com>
2023-11-15 17:35:55 -0300

    update

--- src/script.js
+++ src/script.js
@@ -26,7 +26,10 @@ const quotes = [
     "Você é arte.",
     "I swear I changed my ways for the better 'cause I wanna be with you forever",
     "https://open.spotify.com/embed/track/3dhjNA0jGA8vHBQ1VdD6vV?utm_source=generator",
-    "https://www.youtube.com/embed/LOYueoWBCiU?si=3P29VvdTMa3SA1yH"
+    "https://www.youtube.com/embed/LOYueoWBCiU?si=3P29VvdTMa3SA1yH",
+    "Sua ausência me desespera.",
+    "Você é meu sonho",
+    "I can't seem to not need to need you"
 ];
 
 const nextButton = document.getElementById("nextButton");
